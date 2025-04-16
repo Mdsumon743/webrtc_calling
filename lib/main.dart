32103357bad8d3c9107989d5fc4013b3d21f29b3
myusername <mdsumonhossan743@gmail.com>
2025-04-16 21:46:43 +0600

    push

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:firebase_core/firebase_core.dart' show Firebase;
 import 'package:flutter/material.dart';
 import 'package:webrtc_calling/app.dart' show MyApp;
+import 'package:webrtc_calling/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MyApp());
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,6 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:webrtc_calling/app.dart';
 
 import 'package:webrtc_calling/main.dart';
 
